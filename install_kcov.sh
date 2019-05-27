55b5ef430dd57ae124a4a5f24bd92201b2277f15
Oren Ben-Kiki <oren@ben-kiki.org>
2019-05-28 00:51:12 +0300

    Make only apt-get install of build tools conditional.

--- install_kcov.sh
+++ install_kcov.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 KCOV_VERSION=34
 
 CWD="$PWD"
@@ -10,7 +12,7 @@ cd $KWD
 
 if [ ! -d build ]
 then
-    sudo apt-get install binutils-dev cmake gcc libcurl4-openssl-dev libdw-dev libelf-dev
+    sudo apt-get install cmake gcc
     wget https://github.com/SimonKagstrom/kcov/archive/v$KCOV_VERSION.zip
     unzip v$KCOV_VERSION.zip
     cd kcov-$KCOV_VERSION
