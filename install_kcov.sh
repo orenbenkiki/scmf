8a3e0c3ae7e1bec75e833fca22b6f27a160c13d2
Oren Ben-Kiki <oren@ben-kiki.org>
2019-05-28 01:16:57 +0300

    Fix directory test, clean up leftovers.

--- install_kcov.sh
+++ install_kcov.sh
@@ -7,31 +7,30 @@ KCOV_VERSION=34
 
 CWD="$PWD"
 
-KWD="$HOME/.cache/cargo-make/kcov.$KCOV_VERSION"
-mkdir -p $KWD
-cd $KWD
+KWD="$HOME/.cache/cargo-make/kcov-$KCOV_VERSION"
 
-ls
+mkdir -p $HOME/.cache/cargo-make
+cd $HOME/.cache/cargo-make
+rm -rf kcov.* *.zip* #TODOX
 
-if [ ! -d build ]
+if [ ! -d $KWD/build ]
 then
     sudo apt-get install cmake gcc
     wget https://github.com/SimonKagstrom/kcov/archive/v$KCOV_VERSION.zip
     unzip v$KCOV_VERSION.zip
-    rm v$KCOV_VERSION.zip*
+    rm -rf v$KCOV_VERSION.zip*
     cd kcov-$KCOV_VERSION
 
     mkdir build
 fi
 
-cd ./build
+cd $KWD/build
 cmake ..
 make
 sudo make install
-
 cd $CWD
 
-for OWD in $HOME/.cache/cargo-make/kcov.*
+for OWD in $HOME/.cache/cargo-make/kcov-*
 do
     test "$OWD" = "$KWD" || rm -rf "$OWD"
 done
